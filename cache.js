bf4bcde01a1bf25966cd5cacee8f6302e49fc45d
Hogan jerry <damilolajerryhd@gmail.com>
2018-08-04 16:58:24 +0100

    Portfolio

---
+++ cache.js
@@ -0,0 +1,72 @@
+let CACHE_NAME = 'static-caches';
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) =>{
+      return cache.addAll([
+          '.',
+          'index.html',
+          'css/main.css',
+          'css/uikit.min.css',
+          'js/uikit.min.js',
+          'js/uikit-icons.min.js',
+          'images/pp.jpeg',
+          'images/pb.png'
+        ]);
+    })
+  );
+
+});
+
+
+
+self.addEventListener('activate', (event) =>{
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>{
+      return Promise.all(
+        cacheNames.filter((cacheName) =>{
+          return cacheName.startsWith('static-') && cacheName != CACHE_NAME;
+        }).map((cacheName) =>{
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  // console.log(event.request);
+  let requestUrl = new URL(event.request.url);
+  event.respondWith(
+    caches.match(event.request)
+    .then((response) =>{
+      return response || fetchAndCache(event.request);
+    })
+  );
+});
+
+function fetchAndCache(url) {
+  return fetch(url)
+  .then((response) =>{
+    // Check if we received a valid response
+    if (!response.ok) {
+      throw Error(response.statusText);
+    }
+    return caches.open(CACHE_NAME)
+    .then((cache) =>{
+      cache.put(url, response.clone());
+      return response;
+    });
+  })
+  .catch((error) =>{
+    console.log('Request failed:', error);
+    // You could return a custom offline 404 page here
+
+  });
+}
+
+self.addEventListener('message', function(event) {
+  if (event.data.action === 'skipWaiting') {
+    self.skipWaiting();
+  }
+});
---
+++ sw/index.js
@@ -0,0 +1,54 @@
+
+// "use stricks";
+// import * from 'idb';
+// $('#toaster').hide();
+
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('cache.js')
+  .then(function(reg) {
+    if (!navigator.serviceWorker.controller) {
+      return;
+    }
+
+    if (reg.waiting) {
+      console.log(reg.waiting);
+      updateReady(reg.waiting);
+      return;
+    }
+
+    if (reg.installing) {
+      console.log(reg.installing);
+      trackInstalling(reg.installing);
+      return;
+    }
+
+    reg.addEventListener('updatefound', function() {
+      trackInstalling(reg.installing);
+    });
+  })
+  .catch(function(error) {
+    console.log('Registration failed: ', error);
+  });
+
+  let refreshing;
+  navigator.serviceWorker.addEventListener('controllerchange', function() {
+    if (refreshing) return;
+    window.location.reload();
+    refreshing = true;
+  });
+
+  trackInstalling = (worker) =>{
+    worker.addEventListener('statechange',() =>{
+      if(worker.state == 'installed'){
+        this.updateReady(worker);
+      }
+    });
+
+  };
+
+  updateReady = (worker) =>{
+    
+
+    };
+}
